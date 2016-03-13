6a88d39deca634da0ffa0c75e454e28ce01213e5
shuichifujita <chaashuuu@gmail.com>
2016-03-13 12:23:46 +0900

    section 10.3
    add wc.rb(like unix command "wc")

---
+++ tutorial_02/section_10/wc.rb
@@ -0,0 +1,41 @@
+# coding: utf-8
+
+# ******** ******** 10.1 ******** ******** #
+puts " **** wc (like unix command) **** "
+
+ltotal = 0
+wtotal = 0
+ctotal = 0
+
+ARGV.each do | file |
+  begin
+    input = File.open(file)
+    l = 0
+    w = 0
+    c = 0
+    input.each_line do | line |
+      l += 1
+      c += line.size
+      line.sub(/^\s+/, "")
+      ary = line.split(/\s+/)
+      w += ary.size
+    end
+    input.close
+    printf("%8d %8d %8d %s\n", l, w, c, file)
+    ltotal += l
+    wtotal += w
+    ctotal += c
+  rescue => ex
+    printf ex.message, "\n"
+  end
+end
+
+# ******** ******** ******** ******** #
+# *$$$$$$* *$****$* *$$$$*** ******** #
+# *$****** *$$***$* *$***$** ******** #
+# *$$$$*** *$*$**$* *$****$* ******** #
+# *$****** *$**$*$* *$****$* ******** #
+# *$****** *$***$$* *$***$** ******** #
+# *$$$$$$* *$****$* *$$$$*** *$****** #
+# ******** ******** ******** ******** #
+puts " **** END **** "
