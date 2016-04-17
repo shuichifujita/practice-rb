5698949cb93327638786cf5fa70ad2cfe4cbfddb
shuichifujita <chaashuuu@gmail.com>
2016-04-17 17:55:30 +0900

    tutorial 13.6

--- tutorial_02/section_13/array_column.rb
+++ tutorial_02/section_13/array_column.rb
@@ -10,7 +10,12 @@ sample_array.unshift("unshift")
 sample_array.pop()
 sample_array.push("push")
 
+# これおもしろい/重複した要素を削除する
+# sample_array.uniq!
+test_uniq = sample_array.uniq
+
 p sample_array
+p test_uniq
 
 # ******** ******** ******** ******** #
 # *$$$$$$* *$****$* *$$$$*** ******** #
