f618dc38d6b0f58620e81c3ddd6158fe1ca0341d
shuichifujita <chaashuuu@gmail.com>
2016-09-30 01:20:58 +0900

    edit
    - 素数を数えて落ち着くコードを編集
    - なるはやで落ち着くべく

--- tools/primzahlen.rb
+++ tools/primzahlen.rb
@@ -1,26 +1,47 @@
 # coding: utf-8
 
+# ======== ======== ======== ========
+t = Time.now
+str_t = t.strftime("%H:%M:%S")
+puts "******** ******** START[#{str_t}] ******** ********\n\n"
+
 # 素数を数えて落ち着くコード
 # ======== ======== ======== ========
-puts "******** ******** ******** ********"
 
 class Num
   def initialize(range)
     @rng = range
-    @num = Array.new(range) { |i| i += 1 }
+    @num = (2..range).map { |i| i }
   end
 
-  def rm_even
-    @num.select! do |item|
-      item == 2 || (item != 1 && item % 2 != 0)
-    end
-  end
-
-  # 総当り
   def to_primzahlen
+    prm = []
+    num = @num[0]
+
+    while num <= @rng do
+      # 配列内から最小の素数を抽出
+      prm.push(num)
+
+      # 上記で抽出した素数とその倍数を除去
+      @num.select! { |e| e % num != 0 }
+
+      # 配列の要素がなくなったら抜ける
+      if @num.length == 0
+        break
+      end
+
+      # 次の素数をnumに代入
+      num = @num[0]
+    end
+
+    @num = prm
   end
 end
 
-num = Num.new(10)
+num = Num.new(100000)
 p num.to_primzahlen
+
+# ======== ======== ======== ========
+puts "\n******** ******** END[#{str_t}] ******** ********"
+
 # ======== ======== ======== ========
